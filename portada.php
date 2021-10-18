83ebea2df7d1248f17e1ae2212c509e71a361d87
edupedu101 <edupedu101@eduardvd2001@hotmail.com>
2021-10-18 18:28:51 +0200

    Paginas de portada y juego con css

--- portada.php
+++ portada.php
@@ -2,33 +2,69 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<title></title>
+	<title>Memory - Portada</title>
 	<style type="text/css">
-		
-
+		body{
+			background-color: #211522;
+			color: #d9d21a;
+		}
 		img {
-			display: inline;
+			margin: 0 auto;
+			width: 35%;
+			border-radius: 5px;
 		}
-		div{
+		button{
+			width: 100px;
+			height: 40px;
+			margin-top: 4%;
+			margin-left: auto;
+			margin-right: auto;
+			color: #d9d21a;
+			background-color: #452947;
+			border-color: #613659;
+			border-radius: 3px;
+		}
+		button a{
+			text-decoration: none;
+			color: inherit;
+		}
+		div#instrucciones{
+			border: 5px solid #613659;
+			border-radius: 5px;
+			padding: 15px;
+			width: 45%;
+			background-color: #452947;
+		}
+		h1.titulo{
+			margin: 3% auto 3%;
+		}
+		div.flexbox{
+			display: flex;
+		}
+		.titulo{
+			text-align: center;
+		}
 
-		}
 	</style>
 </head>
 <body>
-	<h1>Bienvienido</h1>
-	<div><h2>Instrucciones</h2><ol>
-		<li>Seleccionar una carta girada boca abajo.</li>
-		<li>Seleccionar la otra carta que creas que tiene la misma imagen que la anterior.</li>
-		<li>Ir emparejando consecutivamente hasta emparejar todas.</li>
-	</ol>
-	<ul>
-		<li>Ten en cuenta los intentos que haces</li>
-		<li>No puedes voltear mas de 2 cartas a la vez</li>
-		<li>Si las 2 cartas volteadas no coinciden, se vuelven a voltean 2 segundos mas tarde.</li>
-	</ul>
+<h1 class="titulo">Memory Tarot</h1>
+<div class="flexbox">
+	<div id="instrucciones"><h2 class="titulo">Instrucciones</h2><ol>
+			<li>Seleccionar una carta girada boca abajo.</li>
+			<li>Seleccionar la otra carta que creas que tiene la misma imagen que la anterior.</li>
+			<li>Ir emparejando consecutivamente hasta emparejar todas.</li>
+		</ol>
+		<ul>
+			<li>Ten en cuenta los intentos que haces</li>
+			<li>No puedes voltear mas de 2 cartas a la vez</li>
+			<li>Si las 2 cartas volteadas no coinciden, se vuelven a voltean 2 segundos mas tarde.</li>
+		</ul>
+	</div>
+	<img src="fotoportada.jpeg" alt="foto de portada">
 </div>
-<img src="fotoportada.jpeg" alt="foto de portada">
-<button><a href="juego.php">Play</a></button>
-
+<div class="flexbox">
+	<button><a href="juego.php">Play</a></button>
+</div>
 </body>
 </html>
