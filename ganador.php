19049aba7fcfd87b77aaa75c80b8c40fe02bfd75
Guillem Albo Pintor <albopintor@gmail.com>
2021-10-22 14:37:39 +0100

    Actualización hall of fame

--- ganador.php
+++ ganador.php
@@ -11,27 +11,28 @@
 <form method="post">
 	<input type="text" placeholder="Nombre" name="jugador">
 	<input type="submit" name="">
+	<?php
+			$contenido=file_get_contents("HallOfFame.txt");
+
+			$nivel=$_GET['lvl'];
+			$time=$_GET['tiempo'];
+			$intentos=$_GET['intentos'];
+			$jugador=$_POST['jugador'];
+
+			$contenido=$contenido.$jugador.";".$nivel.";".$time.";".$intentos."\n";
+
+			if (isset($jugador)) {
+
+				file_put_contents("HallOfFame.txt",$contenido );
+			}
+
+
+	?>
 	
 </form>
 
  
-<?php
-$contenido=file_get_contents("HallOfFame.txt");
 
-$nivel=$_GET['lvl'];
-$time=$_GET['tiempo'];
-$intentos=$_GET['intentos'];
-$jugador=$_POST['jugador'];
-
-$contenido=$contenido.$jugador.";".$nivel.";".$time.";".$intentos."\n";
-
-
-
-file_put_contents("HallOfFame.txt",$contenido );
-
-
-
-?>
 
 
 
